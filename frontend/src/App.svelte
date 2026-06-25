9c973daf25e7a9f4b97cd32e689ee75a45ef6986
dyonak <dyonak@gmail.com>
2026-06-25 17:27:08 -0500

    deploy

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -56,7 +56,7 @@
     flex-direction: column;
     height: 100vh;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
-    font-size: 13px;
+    font-size: 16px;
   }
 
   .tab-bar {
